8286ca6704a4ccb6571371e9dbb38162e2bde31c
GangHan <rhkrrkdgks2@naver.com>
2025-02-13 23:10:05 +0900

    Add Enemy

--- Assets/Undead Survivor/Scripts/Reposition.cs
+++ Assets/Undead Survivor/Scripts/Reposition.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
+    Collider2D coll;
+
+    void Awake()
+    {
+        coll = GetComponent<Collider2D>();
+    }
     void OnTriggerExit2D(Collider2D collision)
     {
         if (!collision.CompareTag("Area"))
@@ -30,8 +36,11 @@ public class Reposition : MonoBehaviour
                     transform.Translate(Vector3.up * dirY * 40);
                 }
                 break;
-            case "Enermy":
-
+            case "Enemy":
+                if (coll.enabled)
+                {
+                    transform.Translate(playerDir * 20 + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f) ,0f));
+                }
                 break;
         }
 
