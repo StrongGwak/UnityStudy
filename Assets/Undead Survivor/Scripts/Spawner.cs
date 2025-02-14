640adf6108ba6cd3081d5faf507fcbc9b1015a9b
GangHan <rhkrrkdgks2@naver.com>
2025-02-14 17:25:18 +0900

    add enemy spawner

---
+++ Assets/Undead Survivor/Scripts/Spawner.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            GameManager.instance.pool.Get(1);
+        }
+    }
+}
