a75410ee9cc81fe0ef07f43ba1a3141b70f74bac
GangHan <rhkrrkdgks2@naver.com>
2025-02-15 23:58:05 +0900

    update spawner

--- Assets/Undead Survivor/Scripts/Spawner.cs
+++ Assets/Undead Survivor/Scripts/Spawner.cs
@@ -6,7 +6,7 @@ public class Spawner : MonoBehaviour
     {
         if (Input.GetButtonDown("Jump"))
         {
-            GameManager.instance.pool.Get(1);
+            GameManager.instance.pool.Get(0);
         }
     }
 }
