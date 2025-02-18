0d9039c7478ff7e9570a3316f605584c948ca50e
GangHan <rhkrrkdgks2@naver.com>
2025-02-18 17:10:31 +0900

    add monster dead and hud

--- Assets/Undead Survivor/Scripts/Enemy.cs
+++ Assets/Undead Survivor/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -10,20 +11,24 @@ public class Enemy : MonoBehaviour
     bool isLive;
 
     Rigidbody2D rigid;
+    Collider2D coll;
     Animator anim;
     SpriteRenderer spriter;
 
+    WaitForFixedUpdate wait;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
         anim = GetComponent<Animator>();
         spriter = GetComponent<SpriteRenderer>();
+        wait = new WaitForFixedUpdate();
     }
 
     void FixedUpdate()
     {
-        if (!isLive)
+        if (!isLive || anim.GetCurrentAnimatorStateInfo(0).IsName("Hit"))
         {
             return;
         }
@@ -42,6 +47,10 @@ public class Enemy : MonoBehaviour
     {
         target = GameManager.instance.player.GetComponent<Rigidbody2D>();
         isLive = true;
+        coll.enabled = true;
+        rigid.simulated = true;
+        spriter.sortingOrder = 2;
+        anim.SetBool("Dead", false);
         health = maxHealth;
     }
 
@@ -55,21 +64,41 @@ public class Enemy : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.CompareTag("Bullet"))
+        if (!collision.CompareTag("Bullet") || !isLive)
         {
             return;
         }
 
         health -= collision.GetComponent<Bullet>().damage;
+        // StartCoroutine("KnockBack");
+        // 문자열로 함수 호출 시 Reflection을 사용하여 해당함수를 찾아내기에 성능 부하가 발생 할 수 있음.
+        StartCoroutine(KnockBack());
 
         if (health > 0)
         {
-
+            anim.SetTrigger("Hit");
         }
         else
         {
-            Dead();
+            isLive = false;
+            // 사망 시 시체가 살아있는 몬스터를 방해하거나 가리지 않게 하기위한 작업
+            coll.enabled = false;
+            rigid.simulated = false;
+            spriter.sortingOrder = 1;
+            anim.SetBool("Dead", true);
+            GameManager.instance.kill++;
+            GameManager.instance.GetExp();
         }
+    }
+
+    IEnumerator KnockBack()
+    {
+        //yield return new WaitForSeconds(2f); // 2초 쉬기
+        // 매번 new 로 생성해서 사용하면 비효율적이기에 변수로 선언해서 사용
+        yield return wait;
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 dirVec = transform.position - playerPos;
+        rigid.AddForce(dirVec.normalized * 3, ForceMode2D.Impulse);
     }
 
     void Dead()
--- Assets/Undead Survivor/Scripts/GameManager.cs
+++ Assets/Undead Survivor/Scripts/GameManager.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-
+    // 인스펙터에서 카테고리를 넣는 효과
+    [Header("# Game Control")]
     public float gameTime;
     public float maxGameTime = 2 * 10f;
-
+    [Header("# Player Info")]
+    public int level;
+    public int kill;
+    public int exp;
+    public int[] nextExp = { 3, 5, 10, 100, 150, 210, 280, 360, 450, 600 };
+    [Header("# Game Object")]
     public Player player;
     public PoolManager pool;
 
@@ -22,6 +28,17 @@ public class GameManager : MonoBehaviour
         if (gameTime > maxGameTime)
         {
             gameTime = maxGameTime;
+        }
+    }
+
+    public void GetExp()
+    {
+        exp++;
+        
+        if (exp == nextExp[level])
+        {
+            level++;
+            exp = 0;
         }
     }
 }
